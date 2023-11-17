a4e3348dfc12b2b1801ddc144acaf15e5cd3180b
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2023-11-17 01:36:44 -0800

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,27 +10,12 @@ repositories {
 }
 
 kotlin {
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
-        hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
-
-    nativeTarget.apply {
+    val platformTargets = listOf(macosX64(), macosArm64())
+    configure(platformTargets) {
         binaries {
             executable {
                 entryPoint = "main"
             }
         }
-    }
-    sourceSets {
-        val nativeMain by getting
-        val nativeTest by getting
     }
 }
