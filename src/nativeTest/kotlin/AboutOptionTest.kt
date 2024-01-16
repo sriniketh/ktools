989faca68729f3e98078935128c4da930ccb2262
Sriniketh Ramachandran <2721128+sriniketh@users.noreply.github.com>
2024-01-15 22:56:05 -0800

    add about option to ktools to list libraries and licenses

---
+++ src/nativeTest/kotlin/AboutOptionTest.kt
@@ -0,0 +1,19 @@
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.testing.test
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AboutOptionTest {
+
+    private val testCommand = TestCommand().aboutOption()
+
+    private class TestCommand : CliktCommand() {
+        override fun run() = Unit
+    }
+
+    @Test
+    fun `aboutOption returns 0 status and exits`() {
+        val result = testCommand.test("--about")
+        assertEquals(0, result.statusCode)
+    }
+}
